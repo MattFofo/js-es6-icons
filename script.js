331f912b55114ea732c3f957435341344b19e100
MattFofo <matteo.segattini2@gmail.com>
2022-03-10 11:16:00 +0100

    bug fix

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
 const eleMain = document.querySelector('.main');
 
-
+const eleIconTypeSelector = document.getElementById('icons-type-selector');
 
 
 //funzione per renderizzare le icons
-function renderIcons(arrIcons, eleContainerIcons) {
+ function renderIcons(arrIcons, eleContainerIcons) {
 
-    eleContainerIcons.innerHTML = '';
+     eleContainerIcons.innerHTML = '';
 
-    //ciclo per scorrere gli oggetti dell'arrey e creare l'elemento box
-    arrStartingIcons.forEach(element => {
-    const eleBox = document.createElement('div');
-    eleBox.classList.add('box');
+     //ciclo per scorrere gli oggetti dell'arrey e creare l'elemento box
+     arrIcons.forEach(element => {
+     const eleBox = document.createElement('div');
+     eleBox.classList.add('box');
 
-    const iconClass = element.family + ' ' + element.prefix + element.name; //ricavo la classe dell'icona
-    eleBox.innerHTML = `
-            <i class="${iconClass}" style="color: ${element.color}"></i>
-            <span>${element.name}<span>
-        `;
+     const iconClass = element.family + ' ' + element.prefix + element.name; //ricavo la classe dell'icona
+     eleBox.innerHTML = `
+             <i class="${iconClass}" style="color: ${element.color}"></i>
+             <span>${element.name}<span>
+         `;
     
-    eleMain.append(eleBox);
-});
-       
-}
+     eleMain.append(eleBox);
+     });
+    
+ }
+
 
 //stampo tutte le icone
 renderIcons(arrStartingIcons, eleMain);
 
 
-const eleIconTypeSelector = document.getElementById('icons-type-selector');
-
 
 eleIconTypeSelector.addEventListener('change', function () {
-    let arrFilteredIcons = [];
+    
     
     const iconTypeSelected = this.value;
     console.log(iconTypeSelected)
 
     if (iconTypeSelected != "all") {
-            arrFilteredIcons = arrStartingIcons.filter((icon) => {
+            const arrFilteredIcons = arrStartingIcons.filter((icon) => {
             if (icon.type == iconTypeSelected) {
                 return true;
                 
-            }
-            
+            }    
         });
         renderIcons(arrFilteredIcons, eleMain);    
         
     }else {
         renderIcons(arrStartingIcons, eleMain);
     }
-})
+
+});
+
